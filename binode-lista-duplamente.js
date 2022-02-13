965e0cfb6520390099848bb7f5b211ab47222449
Rafael Rocha <rafaelrocha@outlook.com>
2022-02-13 11:33:59 -0300

    Árvore montada conforme instruções

--- binode-lista-duplamente.js
+++ binode-lista-duplamente.js
@@ -1,7 +1,22 @@
 class No {
-    constructor(conteudo) {
-        this.conteudo = conteudo;
-        this.esquerda = null;
-        this.direita = null;
-    }
+  constructor(conteudo) {
+    this.conteudo = conteudo;
+    this.esquerda = null;
+    this.direita = null;
+  }
+}
+
+function main() {
+  const noPrincipal = new No(4);
+  noPrincipal.esquerda = new No(2);
+  noPrincipal.direita = new No(5);
+
+  noPrincipal.esquerda.esquerda = new No(1);
+  noPrincipal.esquerda.direita = new No(3);
+
+  noPrincipal.direita.direita = new No(6);
+
+  noPrincipal.esquerda.esquerda.esquerda = new No(0);
+
+  const converte = (noPrincipal) => {};
 }
